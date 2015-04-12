5840a435626ce02fa71e926ba1a5639226026ccb
Ian Paterson <ipaterso@adobe.com>
2015-04-12 16:11:30 +0100

    Fix the issue with template_directory resolving to dms, needs to be to the childtheme, which is done via stylesheet_directory

--- footer.php
+++ footer.php
@@ -7,7 +7,43 @@
 
 
     <div class="site-info">
-
+       <div class="no-more-tables ipt_padded_table">
+         <table >
+          <tbody>
+            <tr>
+              <td class="ipt_padded_both ipt_semi_opaque"> 
+                
+                <img src="<?php bloginfo('stylesheet_directory'); ?>/images/tick-logo.jpg" alt="" width="129" height="57">
+                    
+                <img src="<?php bloginfo('stylesheet_directory'); ?>/images/virgin-money-giving-logo.jpg" alt="" width="88" height="62">
+                   
+              </td>
+              <td class="ipt_padded_table_cell ipt_ruled_above ipt_ruled_left"> 
+                <div class="ipt_text_11_135_15">
+                 <p>It's Good 2 Give&#8482; is a registered charity no. Sc041416. Company limited by guarantee no. SC376977</p>
+                 <a class="ipt_a_135" href="#">Terms &amp; Conditions</a> &nbsp; &nbsp; | &nbsp; &nbsp; <a  class="ipt_a_135" href="#">Privacy &amp; Cookies Policy</a> 
+                </div>
+                <div class="ipt_text_11_135_11">
+                  <p><a  class="ipt_a_135"  href="#">Contact Us</a> &nbsp;&nbsp;
+                    <img  class="ipt_envelope" src="<?php bloginfo('template_directory'); ?>/images/big%20envelope-vector%20smart%20object.png" /></p> 
+                  <p>Share this page&nbsp;&nbsp;
+                  <img  class="ipt_social_18" src="<?php bloginfo('template_directory'); ?>/images/grey%20facebook%20icon%20big-vector%20smart%20object.png" />
+                  &nbsp;&nbsp;<img  class="ipt_social_18" src="<?php bloginfo('template_directory'); ?>/images/grey%20tumblr%20icon%20big-vector%20smart%20object.png" />
+                  &nbsp;&nbsp;<img  class="ipt_social_18" src="<?php bloginfo('template_directory'); ?>/images/grey%20youtube%20icon%20big-vector%20smart%20object.png" />
+                  &nbsp;&nbsp;<img  class="ipt_social_18" src="<?php bloginfo('template_directory'); ?>/images/grey%20pinterest%20icon%20big-vector%20smart%20object.png" />
+                   </p>
+                </div>
+              </td>
+              <td class="ipt_padded_table_cell "> 
+                <div class="ipt_text_11_135_17">
+                  <p>It's Good 2 Give&#8482; raises money to help and support young people with cancer and their families. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.</p>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+<!--
       <div class="rTable "> 
         <div class="rTableRow"> 
         
@@ -54,9 +90,9 @@
         </div> 
         
         
-        </div> <!-- table row -->
-      </div>  <!-- table itself -->
-
+        </div> 
+      </div>  
+-->
 
 
     <div class="ipt_redline ipt_spaced_above"><!-- simple frame --></div>
