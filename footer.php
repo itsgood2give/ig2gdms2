72a4eae804daa2d932699adcf4e1e6b59f641a25
Ian Paterson <ipaterso@adobe.com>
2015-04-14 19:30:37 +0100

    remove some placehlder lorem ipsum

--- footer.php
+++ footer.php
@@ -25,75 +25,27 @@
                 </div>
                 <div class="ipt_text_11_135_11">
                   <p><a  class="ipt_a_135"  href="#">Contact Us</a> &nbsp;&nbsp;
-                    <img  class="ipt_envelope" src="<?php bloginfo('template_directory'); ?>/images/big%20envelope-vector%20smart%20object.png" /></p> 
+                    <img  class="ipt_envelope" src="<?php bloginfo('stylesheet_directory'); ?>/images/big%20envelope-vector%20smart%20object.png" /></p> 
                   <p>Share this page&nbsp;&nbsp;
-                  <img  class="ipt_social_18" src="<?php bloginfo('template_directory'); ?>/images/grey%20facebook%20icon%20big-vector%20smart%20object.png" />
-                  &nbsp;&nbsp;<img  class="ipt_social_18" src="<?php bloginfo('template_directory'); ?>/images/grey%20tumblr%20icon%20big-vector%20smart%20object.png" />
-                  &nbsp;&nbsp;<img  class="ipt_social_18" src="<?php bloginfo('template_directory'); ?>/images/grey%20youtube%20icon%20big-vector%20smart%20object.png" />
-                  &nbsp;&nbsp;<img  class="ipt_social_18" src="<?php bloginfo('template_directory'); ?>/images/grey%20pinterest%20icon%20big-vector%20smart%20object.png" />
+                  <img  class="ipt_social_18" src="<?php bloginfo('stylesheet_directory'); ?>/images/grey%20facebook%20icon%20big-vector%20smart%20object.png" />
+                  &nbsp;&nbsp;<img  class="ipt_social_18" src="<?php bloginfo('stylesheet_directory'); ?>/images/grey%20tumblr%20icon%20big-vector%20smart%20object.png" />
+                  &nbsp;&nbsp;<img  class="ipt_social_18" src="<?php bloginfo('stylesheet_directory'); ?>/images/grey%20youtube%20icon%20big-vector%20smart%20object.png" />
+                  &nbsp;&nbsp;<img  class="ipt_social_18" src="<?php bloginfo('stylesheet_directory'); ?>/images/grey%20pinterest%20icon%20big-vector%20smart%20object.png" />
                    </p>
                 </div>
               </td>
-              <td class="ipt_padded_table_cell "> 
+              <td class="ipt_padded_table_cell_right "> 
                 <div class="ipt_text_11_135_17">
-                  <p>It's Good 2 Give&#8482; raises money to help and support young people with cancer and their families. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.</p>
+                  <p>It's Good 2 Give&#8482; raises money to help and support young people with cancer and their families. As our
+name makes clear, we believe that giving should be as easy and as enjoyable as possible.
+You can donate money, time, even a good idea. All are welcome. All will make a
+difference. </p>
                 </div>
               </td>
             </tr>
           </tbody>
         </table>
       </div>
-<!--
-      <div class="rTable "> 
-        <div class="rTableRow"> 
-        
-          <div class="rTableCell"> 
-
-              <div class="tinyTable ipt_offset_down"> 
-                <div class="tinyTableRow ipt_semi_opaque"> 
-                  <div class="tinyTableCell"> 
-                    <img src="<?php bloginfo('template_directory'); ?>/images/tick-logo.jpg" alt="" width="129" height="57">
-                  </div> 
-                  <div class="tinyTableCell"> 
-                      <img id="u255_img" src="<?php bloginfo('template_directory'); ?>/images/virgin-money-giving-logo.jpg" alt="" width="88" height="62">
-                  </div> 
-              </div>
-            </div>
-          </div> 
-           
-      
-          <div class="rTableCell ipt_decorated_left">
-
-            <div class="ipt_text_11_135_15">
-               <p>It's Good 2 Give&#8482; is a registered charity no. Sc041416. Company limited by guarantee no. SC376977</p>
-               <a class="ipt_a_135" href="#">Terms &amp; Conditions</a> &nbsp; &nbsp; | &nbsp; &nbsp; <a  class="ipt_a_135" href="#">Privacy &amp; Cookies Policy</a> 
-            </div>
-
-            <div class="ipt_text_11_135_11">
-              <p><a  class="ipt_a_135"  href="#">Contact Us</a> &nbsp;&nbsp;
-                <img  class="ipt_envelope" src="<?php bloginfo('template_directory'); ?>/images/big%20envelope-vector%20smart%20object.png" /></p> 
-              <p>Share this page&nbsp;&nbsp;
-              <img  class="ipt_social_18" src="<?php bloginfo('template_directory'); ?>/images/grey%20facebook%20icon%20big-vector%20smart%20object.png" />
-              &nbsp;&nbsp;<img  class="ipt_social_18" src="<?php bloginfo('template_directory'); ?>/images/grey%20tumblr%20icon%20big-vector%20smart%20object.png" />
-              &nbsp;&nbsp;<img  class="ipt_social_18" src="<?php bloginfo('template_directory'); ?>/images/grey%20youtube%20icon%20big-vector%20smart%20object.png" />
-              &nbsp;&nbsp;<img  class="ipt_social_18" src="<?php bloginfo('template_directory'); ?>/images/grey%20pinterest%20icon%20big-vector%20smart%20object.png" />
-               </p>
-            </div>
-          
-          </div> 
-      
-      
-        <div class="rTableCell">
-          <div class="ipt_text_11_135_17">
-                 <p>It's Good 2 Give&#8482; raises money to help and support young people with cancer and their families. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.</p>
-          </div>
-        </div> 
-        
-        
-        </div> 
-      </div>  
--->
-
 
     <div class="ipt_redline ipt_spaced_above"><!-- simple frame --></div>
 
