f13b4fd483d6fac0228fe6795690b57b26be1eb9
Ian Paterson <ipaterso@adobe.com>
2015-08-12 23:46:21 +0100

    Tweaklets

--- footer.php
+++ footer.php
@@ -7,6 +7,27 @@
 
 
     <div class="site-info">
+      <div class="content-column one_third ipt_semi_opaque">
+         <img src="<?php bloginfo('stylesheet_directory'); ?>/images/tick-logo.jpg" alt="" >          
+          <img src="<?php bloginfo('stylesheet_directory'); ?>/images/virgin-money-giving-logo.jpg" alt="" >   
+</div>
+<div class="content-column one_third">
+  <div class="ipt_text_11_135_17">
+     <p>It's Good 2 Give&#8482; is a registered charity no. Sc041416. Company limited by guarantee no. SC376977</p>
+    <a class="ipt_a_135" href="#">Terms &amp; Conditions</a> &nbsp; &nbsp; | &nbsp; &nbsp; <a  class="ipt_a_135" href="#">Privacy &amp; Cookies Policy</a>      
+  </div>
+</div>
+<div class="content-column one_third last_column">
+  <div class="ipt_text_11_135_17">
+      <p>It's Good 2 Give&#8482; raises money to help and support young people with cancer and their families. As our
+    name makes clear, we believe that giving should be as easy and as enjoyable as possible.
+    You can donate money, time, even a good idea. All are welcome. All will make a
+    difference. </p>
+  </div>
+</div>
+<div class="clear_column"></div>
+
+      <!--
        <div class="no-more-tables ipt_padded_table">
          <table >
           <tbody>
@@ -26,12 +47,14 @@
                 <div class="ipt_text_11_135_11">
                   <p><a  class="ipt_a_135"  href="#">Contact Us</a> &nbsp;&nbsp;
                     <img  class="ipt_envelope" src="<?php bloginfo('stylesheet_directory'); ?>/images/big%20envelope-vector%20smart%20object.png" /></p> 
-                  <p>Share this page&nbsp;&nbsp;
+               
+               <p>Share this page&nbsp;&nbsp;
                   <img  class="ipt_social_18" src="<?php bloginfo('stylesheet_directory'); ?>/images/grey%20facebook%20icon%20big-vector%20smart%20object.png" />
                   &nbsp;&nbsp;<img  class="ipt_social_18" src="<?php bloginfo('stylesheet_directory'); ?>/images/grey%20tumblr%20icon%20big-vector%20smart%20object.png" />
                   &nbsp;&nbsp;<img  class="ipt_social_18" src="<?php bloginfo('stylesheet_directory'); ?>/images/grey%20youtube%20icon%20big-vector%20smart%20object.png" />
                   &nbsp;&nbsp;<img  class="ipt_social_18" src="<?php bloginfo('stylesheet_directory'); ?>/images/grey%20pinterest%20icon%20big-vector%20smart%20object.png" />
                    </p>
+            
                 </div>
               </td>
               <td class="ipt_padded_table_cell_right ipt_padded_both"> 
@@ -45,6 +68,7 @@ difference. </p>
             </tr>
           </tbody>
         </table>
+      -->
       </div>
 
     <div class="ipt_redline ipt_spaced_above"><!-- simple frame --></div>
