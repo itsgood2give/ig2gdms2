ef071badd327585dd400d64d50b5d43b907ad871
Ian Paterson <ipaterso@adobe.com>
2015-08-18 23:35:30 +0100

    small updates

--- footer.php
+++ footer.php
@@ -3,82 +3,48 @@
 
 
 ?>
-  <div class="ipt_1px_black ipt_spaced_below"></div>
-
-
-    <div class="site-info">
-      <div class="content-column one_third ipt_semi_opaque">
-         <img src="<?php bloginfo('stylesheet_directory'); ?>/images/tick-logo.jpg" alt="" >          
-          <img src="<?php bloginfo('stylesheet_directory'); ?>/images/virgin-money-giving-logo.jpg" alt="" >   
-</div>
-<div class="content-column one_third">
-  <div class="ipt_text_11_135_17">
-     <p>It's Good 2 Give&#8482; is a registered charity no. Sc041416. Company limited by guarantee no. SC376977</p>
-    <a class="ipt_a_135" href="#">Terms &amp; Conditions</a> &nbsp; &nbsp; | &nbsp; &nbsp; <a  class="ipt_a_135" href="#">Privacy &amp; Cookies Policy</a>      
+  <div class="ipt_1px_black ipt_spaced_below">
   </div>
-</div>
-<div class="content-column one_third last_column">
-  <div class="ipt_text_11_135_17">
-      <p>It's Good 2 Give&#8482; raises money to help and support young people with cancer and their families. As our
-    name makes clear, we believe that giving should be as easy and as enjoyable as possible.
-    You can donate money, time, even a good idea. All are welcome. All will make a
-    difference. </p>
-  </div>
-</div>
-<div class="clear_column"></div>
 
-      <!--
-       <div class="no-more-tables ipt_padded_table">
-         <table >
-          <tbody>
-            <tr>
-              <td class="ipt_padded_both ipt_semi_opaque"> 
-                
-                <img src="<?php bloginfo('stylesheet_directory'); ?>/images/tick-logo.jpg" alt="" width="129" height="57">
-                    
-                <img src="<?php bloginfo('stylesheet_directory'); ?>/images/virgin-money-giving-logo.jpg" alt="" width="88" height="62">
-                   
-              </td>
-              <td class="ipt_padded_table_cell ipt_ruled_above ipt_ruled_left"> 
-                <div class="ipt_text_11_135_15">
-                 <p>It's Good 2 Give&#8482; is a registered charity no. Sc041416. Company limited by guarantee no. SC376977</p>
-                 <a class="ipt_a_135" href="#">Terms &amp; Conditions</a> &nbsp; &nbsp; | &nbsp; &nbsp; <a  class="ipt_a_135" href="#">Privacy &amp; Cookies Policy</a> 
-                </div>
-                <div class="ipt_text_11_135_11">
-                  <p><a  class="ipt_a_135"  href="#">Contact Us</a> &nbsp;&nbsp;
-                    <img  class="ipt_envelope" src="<?php bloginfo('stylesheet_directory'); ?>/images/big%20envelope-vector%20smart%20object.png" /></p> 
-               
-               <p>Share this page&nbsp;&nbsp;
-                  <img  class="ipt_social_18" src="<?php bloginfo('stylesheet_directory'); ?>/images/grey%20facebook%20icon%20big-vector%20smart%20object.png" />
-                  &nbsp;&nbsp;<img  class="ipt_social_18" src="<?php bloginfo('stylesheet_directory'); ?>/images/grey%20tumblr%20icon%20big-vector%20smart%20object.png" />
-                  &nbsp;&nbsp;<img  class="ipt_social_18" src="<?php bloginfo('stylesheet_directory'); ?>/images/grey%20youtube%20icon%20big-vector%20smart%20object.png" />
-                  &nbsp;&nbsp;<img  class="ipt_social_18" src="<?php bloginfo('stylesheet_directory'); ?>/images/grey%20pinterest%20icon%20big-vector%20smart%20object.png" />
-                   </p>
-            
-                </div>
-              </td>
-              <td class="ipt_padded_table_cell_right ipt_padded_both"> 
-                <div class="ipt_text_11_135_17">
-                  <p>It's Good 2 Give&#8482; raises money to help and support young people with cancer and their families. As our
-name makes clear, we believe that giving should be as easy and as enjoyable as possible.
-You can donate money, time, even a good idea. All are welcome. All will make a
-difference. </p>
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      -->
-      </div>
 
-    <div class="ipt_redline ipt_spaced_above"><!-- simple frame --></div>
-
-    <div >
-     <p id="u246-2">no-one has ever become poor by giving</p>
-     <p id="u246-4">-Anne Frank</p>
+  <div class="site-info">
+    
+    <div class="content-column one_third ipt_semi_opaque">
+       <img src="<?php bloginfo('stylesheet_directory'); ?>/images/clear.png" alt="" >          
+        <img src="<?php bloginfo('stylesheet_directory'); ?>/images/tick-logo.jpg" alt="" >          
+        <img src="<?php bloginfo('stylesheet_directory'); ?>/images/virgin-money-giving-logo.jpg" alt="" >   
     </div>
 
-</div>
+    <div class="content-column one_third">
+      <div class="ipt_text_11_135_17">
+         <p>It's Good 2 Give&#8482; is a registered charity no. Sc041416. Company limited by guarantee no. SC376977</p>
+        <a class="ipt_a_135" href="#">Terms &amp; Conditions</a> &nbsp; &nbsp; | &nbsp; &nbsp; <a  class="ipt_a_135" href="#">Privacy &amp; Cookies Policy</a>      
+      </div>
+    </div>
+
+    <div class="content-column one_third last_column">
+      <div class="ipt_text_11_135_17">
+          <p>It's Good 2 Give&#8482; raises money to help and support young people with cancer and their families. As our
+        name makes clear, we believe that giving should be as easy and as enjoyable as possible.
+        You can donate money, time, even a good idea. All are welcome. All will make a
+        difference. </p>
+      </div>
+    </div>
+
+    <div class="clear_column">
+    </div>
+
+  </div> <!-- end of site-info -->
+
+
+  <div class="ipt_redline ipt_spaced_above"><!-- simple frame --></div>
+
+  <div >
+     <p id="u246-2">no-one has ever become poor by giving</p>
+     <p id="u246-4">-Anne Frank</p>
+  </div>
+
+<!-- end of custom footer code -->
  
 <?php
 
