bda3c60196409f668ed2aedb3ed3e510b74cf82c
Ian Paterson <ipaterso@adobe.com>
2016-04-21 23:18:35 +0100

    Updates to reflect latest design

--- footer.php
+++ footer.php
@@ -3,6 +3,7 @@
 
 
 ?>
+<!--
   <div class="ipt_1px_black ipt_spaced_below">
   </div>
 
@@ -34,10 +35,10 @@
     <div class="clear_column">
     </div>
 
-  </div> <!-- end of site-info -->
+  </div> 
 
 
-  <div class="ipt_redline ipt_spaced_above"><!-- simple frame --></div>
+  <div class="ipt_redline ipt_spaced_above"></div>
 
 <div class="generic-info">
 
@@ -51,11 +52,62 @@
   </div>
 
 </div>
+-->
 
-<!-- end of custom footer code -->
- 
+<section id="pl_areauto7hie" data-object="PLSectionArea" data-sid="pl_area" data-clone="uto7hie" class="pl-area pl-area-sortable area-tag    pl-bg-cover section-pl_area" style="  "><div class="pl-area-pad fix">  <div class="pl-area-wrap " style="padding-top: 20px; padding-bottom: 20px;">
 
+    <div class="pl-content nested-section-area">
+            <div class="pl-inner area-region pl-sortable-area editor-row" style="">
+        <div class="row grid-row"></div>
 
+<div class="row grid-row">
+
+<!-- Column | Section Template -->
+<section id="plcolumnu1afc2" data-object="PLColumn" data-sid="plcolumn" data-clone="u1afc2" class="pl-section span4 offset0 force-start-row    pl-bg-cover section-plcolumn" style="  "><div class="pl-section-pad fix">    <div class="pl-sortable-column pl-sortable-area editor-row ">
+
+      <div class="row grid-row">
+
+<!-- TextBox | Section Template -->
+<section id="textboxuwow5bo" data-object="PageLinesTextBox" data-sid="textbox" data-clone="uwow5bo" class="pl-section span12 offset0    pl-bg-cover section-textbox" style="  "><div class="pl-section-pad fix"><div class="textbox-wrap pl-animation   animation-loaded" style="font-size: 11px;"><div class="hentry" data-sync="textbox_content"><div class="ipt_semi_opaque">
+<img src="/wp-content/themes/ig2gdms2/images/tick-logo.jpg" alt="FRSB -  give with confidence"> <img src="/wp-content/themes/ig2gdms2/images/virgin-money-giving-logo.jpg" alt="Virgin -  moneygiving">
+</div>
+</div></div></div></section></div>      <span class="pl-column-forcer">&nbsp;</span>
+    </div>
+  </div></section>
+
+<!-- Column | Section Template -->
+<section id="plcolumnu1b0d7" data-object="PLColumn" data-sid="plcolumn" data-clone="u1b0d7" class="pl-section span4 offset0    pl-bg-cover section-plcolumn" style="  "><div class="pl-section-pad fix">    <div class="pl-sortable-column pl-sortable-area editor-row ">
+
+      <div class="row grid-row">
+
+<!-- TextBox | Section Template -->
+<section id="textboxuqpfa2l" data-object="PageLinesTextBox" data-sid="textbox" data-clone="uqpfa2l" class="pl-section span12 offset0    pl-bg-cover section-textbox" style="  "><div class="pl-section-pad fix"><div class="textbox-wrap pl-animation   animation-loaded" style="font-size: 11px;"><div class="hentry" data-sync="textbox_content"><div class="ipt_text_11_135_17">
+<p>It's Good 2 Give is a registered charity no. Sc041416. </p>
+<p>Company limited by guarantee no. SC376977</p>
+<p><a class="ipt_a_135" href="/terms-and-conditions">Terms &amp; Conditions</a> &nbsp; &nbsp; | &nbsp; &nbsp; <a class="ipt_a_135" href="/privacy-and-cookies-policy">Privacy &amp; Cookies Policy</a>
+</p></div>
+</div></div></div></section></div>      <span class="pl-column-forcer">&nbsp;</span>
+    </div>
+  </div></section>
+
+<!-- Column | Section Template -->
+<section id="plcolumnu1b1a3" data-object="PLColumn" data-sid="plcolumn" data-clone="u1b1a3" class="pl-section span4 offset0    pl-bg-cover section-plcolumn" style="  "><div class="pl-section-pad fix">    <div class="pl-sortable-column pl-sortable-area editor-row ">
+
+      <div class="row grid-row">
+
+<!-- TextBox | Section Template -->
+<section id="textboxupxc8dw" data-object="PageLinesTextBox" data-sid="textbox" data-clone="upxc8dw" class="pl-section span12 offset0    pl-bg-cover section-textbox" style="  "><div class="pl-section-pad fix"><div class="textbox-wrap pl-animation   animation-loaded" style="font-size: 11px;"><div class="hentry" data-sync="textbox_content"><div class="ipt_text_11_135_17">
+<p>It's Good 2 Give raises money to help and support young people with cancer and their families. As our<br>
+name makes clear, we believe that giving should be as easy and as enjoyable as possible.<br>
+You can donate money, time, even a good idea. All are welcome. All will make a<br>
+difference. </p>
+</div>
+</div></div></div></section></div>      <span class="pl-column-forcer">&nbsp;</span>
+    </div>
+  </div></section></div>      </div>
+    </div>
+  </div>
+  </div></section>
 
 <?php
 
